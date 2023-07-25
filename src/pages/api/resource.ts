3060a7aaca138eb54ac20f03f9d9bfcaa91945b4
Eli Gundry <eligundry@gmail.com>
2023-07-25 18:32:27 -0400

    Alright, let's see if this works on Netlify

--- src/pages/api/resource.ts
+++ src/pages/api/resource.ts
@@ -2,14 +2,20 @@ import type { APIRoute } from "astro";
 
 export const prerender = false;
 
-export const get: APIRoute = () => {
+export const get: APIRoute = async () => {
   return {
     body: "Hello from the API!",
   };
 };
 
-export const post: APIRoute = () => {
-  return {
-    body: "You posted to the API!",
-  };
+export const post: APIRoute = async ({ request }) => {
+  const data = await request.formData();
+  const file = data.get("file");
+  if (!file) {
+    return new Response("No file uploaded", { status: 400 });
+  }
+
+  return new Response(`Hello from the API! You uploaded ${file?.name}`, {
+    status: 200,
+  });
 };
